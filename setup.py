6fd343b8fed6196fe7ca633ee1e7a0bcfa225acc
Harry <hjwp2@cantab.net>
2018-07-03 09:49:29 +0100

    bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='pytest-contexts',
-    version='0.1.0',
+    version='0.1.1',
     author='Harry Percival',
     author_email='obeythetestinggoat@gmail.com',
     maintainer='Harry Percival',
